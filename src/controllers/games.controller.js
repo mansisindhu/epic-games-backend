7aaec3bfe3be60d38e1fdb1224201be1c765329d
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-17 16:48:44 +0530

    Filters and sorting api created

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -13,6 +13,64 @@ router.get("/", async (_, res) => {
   }
 });
 
+router.get("/filters", async (req, res) => {
+  try {
+    const price = req.query.price;
+    let genre = req.query.genre?.split(",");
+    let features = req.query.features?.split(",");
+    const sortBy = req.query.sortBy;
+
+    features = features || [
+      "single-player",
+      "multiplayer",
+      "controller-support",
+      "co-op",
+      "competitive",
+    ];
+
+    genre = genre || [
+      "Action",
+      "Fighting",
+      "Indie",
+      "Puzzle",
+      "Strategy",
+      "Horror",
+      "Survival",
+      "Casual",
+      "Shooter",
+      "Adventure",
+    ];
+
+    const underMinAmount = parseInt(price) || 100000000;
+
+    let data = await Games.find({
+      $and: [
+        { genres: { $in: genre } },
+        { features: { $in: features } },
+        { "price.mainPrice": { $lt: underMinAmount } },
+      ],
+    });
+
+    if (sortBy === "alphabetical") {
+      data.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortBy === "highToLow") {
+      data.sort((a, b) => b.price.mainPrice - a.price.mainPrice);
+    } else if (sortBy === "lowToHigh") {
+      data.sort((a, b) => a.price.mainPrice - b.price.mainPrice);
+    } else if (sortBy === "newRelease") {
+      data.sort((date1, date2) => {
+        date1 = new Date(date1.releaseDate);
+        date2 = new Date(date2.releaseDate);
+        return date2 - date1;
+      });
+    }
+
+    res.send({ data });
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
 router.get("/:id", async (req, res) => {
   try {
     const game = await Games.findById(req.params.id).lean().exec();
