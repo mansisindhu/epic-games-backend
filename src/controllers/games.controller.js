dfd017e3ca6ecddd79a6bfb760e3d9e189dd062f
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-17 13:28:59 +0530

    Added api to get game data by id

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -3,13 +3,23 @@ const router = express.Router();
 
 const Games = require("../models/game.model");
 
-router.get("/", async (req, res) => {
+router.get("/", async (_, res) => {
   try {
     const games = await Games.find({}).lean().exec();
 
     res.status(200).send({ games });
   } catch (err) {
     res.send(500).send({ err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const game = await Games.findById(req.params.id).lean().exec();
+
+    res.status(200).send(game);
+  } catch (err) {
+    res.status(404).send({ err });
   }
 });
 
